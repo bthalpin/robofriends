64ebace82f6983428b99e6795f26989771fab31f
bthalpin <brian.t.halpin@gmail.com>
2021-07-16 09:12:12 -0400

    redux

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Searchbox = ({searchfield,searchChange}) => {
+const Searchbox = ({searchField,searchChange}) => {
     return (
         <div className = 'pa2'>
             <input className = 'pa3 ba b--green bg-lightest-blue' type = 'search' placeholder = 'Search robots' onChange = {searchChange}></input>
